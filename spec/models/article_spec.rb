76693d47477f90333b33897739fab2a6d95d3a4a
SebastianN97 <sebbe132132@gmail.com>
2020-09-08 19:07:00 +0200

    Solving spelling mistakes in article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,6 @@
 
 RSpec.describe Article, type: :model do
-    describe 'DB taböe' do
+    describe 'DB table' do
         it { is_expected.to have_db_column :id }
         it { is_expected.to have_db_column :title }
     end
